4a582803d051b169dd9f8518bf20cd6deba0368c
Mikhail <mihaershv@gmail.com>
2022-02-27 19:06:41 +0300

    fixes, get projects

--- app/app.py
+++ app/app.py
@@ -3,20 +3,32 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 
-
 application = Flask(__name__)
 
-application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE'] + '?authSource=admin'
 
 mongo = PyMongo(application)
 db = mongo.db
 
 
-@application.route('/')
-def index():
+@application.route('/projects')
+def projects():
+    projects = db.projects.find()
+
+    data = []
+    for project in projects:
+        item = {
+            'id': project['id'],
+            'name': project['name'],
+            'description': project['description'],
+            'images_count': project['images_count'],
+            'metadata_size': project['metadata_size'],
+        }
+        data.append(item)
+
     return jsonify(
         status=True,
-        message='Welcome to meshroom!'
+        data=data,
     )
 
 
